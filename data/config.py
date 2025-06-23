734425247551fcde68837c643e92aba73cdf4ded
kolade-esm <kolade@esmglobalconsulting.com>
2025-06-23 14:03:49 +0100

    streamlit env virable

--- data/config.py
+++ data/config.py
@@ -2,5 +2,5 @@ config = {
     'pidgin_collection':"english_nigerianpidgin",
     'swahili_collection': "english_swahili",
     'yoruba_collection': "english_yoruba",
-    'client': ""
+    'client': "mongodb+srv://kolade:bM0MkpVxYu9qMhAc@nkenne-cluster.zbsi9.mongodb.net/"
 }
