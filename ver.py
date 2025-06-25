1ea37a35042385ff6833ff4d3e892dc53d0e1d41
kolade-esm <kolade@esmglobalconsulting.com>
2025-06-25 13:27:52 +0100

    Removed .devcontainer from repo

--- ver.py
+++ ver.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import pandas as pd
-import shutil
-import subprocess
 # import os
 import json
 import pymongo
-import argparse
 
 import pandas as pd
-import glob
-import argparse
+import matplotlib.pyplot as plt
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import matplotlib
+import pydrive
+import sys
 
 
 print("ver.py loaded")
@@ -19,3 +20,6 @@ print(f"streamlit version: {st.__version__}")
 print(f"Pandas version: {pd.__version__}")
 print(f"json version: {json.__version__}")
 print(f"pymongo version: {pymongo.__version__}")
+print(f"matplotlib version: {matplotlib.__version__}")
+# print(f"pydrive version: {pydrive.__version__}")
+print(f"Python version: {sys.version}")
