f133565e6f622a56e467983252bccf779bdf6638
kolade-esm <kolade@esmglobalconsulting.com>
2025-06-25 16:15:29 +0100

    update auth

--- config/auth.py
+++ config/auth.py
@@ -13,9 +13,10 @@ client_config = {
         "client_id": st.secrets["google_drive"]["client_id"],
         "project_id":"my-app-463810",
         "client_secret": st.secrets["google_drive"]["client_secret"],
-        "redirect_uris": ["https://nkenne-validator-tool-main-hhk3wc.streamlit.app/"],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token"
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "redirect_uris":st.secrets["google_drive"]["redirect_uris"],
         
     }
 }
